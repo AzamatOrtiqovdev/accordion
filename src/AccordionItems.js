1e3b589e04608c564b41f86a2e17d846fa5cc80b
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2024-07-31 22:36:32 +0500

    new thing

--- src/AccordionItems.js
+++ src/AccordionItems.js
@@ -4,12 +4,19 @@ export default function AccordionItems({ title, text, number }) {
   const [isOpen, setIsOPen] = useState(false);
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={() => setIsOPen(!isOpen)}>
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => setIsOPen(!isOpen)}
+    >
       <p className="number">{number < 10 ? `0${number + 1}` : number + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
 
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && (
+        <div className="content-box">
+          <p>{text}</p>
+        </div>
+      )}
     </div>
   );
 }
